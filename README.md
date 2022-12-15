4f946f5ae840187b4929ecbac5881d3a385b7442
lmm333 <lmm333@126.com>
2022-12-15 13:02:22 +0800

    ADD: 知乎数据

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # 2023
 创建一个项目，用自动化的方式，记录我的2023年
+
+## 知乎
+- 功能：通过 API 获取用户信息，写入文件，每日定时触发
+- 脚本： zhihu.py
+- 参考项目: https://github.com/guodongxiaren/py/tree/master/zhihu
+- API文档：zhihu API v4: https://juejin.cn/post/6844903780199170062
+
+```
+用户信息
+    
+URL：https://www.zhihu.com/api/v4/members/{un}
+参数：include：额外信息，用逗号分割，包括
+locations,employments,gender,educations,business,voteup_count,thanked_Count,follower_count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,following_columns_count,avatar_hue,answer_count,articles_count,pins_count,question_count,columns_count,commercial_question_count,favorite_count,favorited_count,logs_count,included_answers_count,included_articles_count,included_text,message_thread_token,account_status,is_active,is_bind_phone,is_force_renamed,is_bind_sina,is_privacy_protected,sina_weibo_url,sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,is_org_createpin_white_user,mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage,badge[?(type=best_answerer)].topics
+```
---
+++ zhihu.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding=utf-8
+import datetime
+import requests
+import json
+
+USER_NAME = 'mm.lou'
+FILE_NAME_DATA = 'zhihu_data.csv'
+FILE_NAME_INFO = 'zhihu_info.json'
+
+header = {
+    'Host': 'www.zhihu.com',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': '*/*',
+    'User-Agent': 'PostmanRuntime/7.17.1',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+}
+
+url = f'https://www.zhihu.com/api/v4/members/{USER_NAME}?include=' \
+      f'follower_count,voteup_count,locations,badge,locations,employments,gender,educations,business,' \
+      f'thanked_Count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,' \
+      f'following_columns_count,avatar_hue,answer_count,articles_count,pins_count,question_count,columns_count,commercial_question_count,' \
+      f'favorite_count,favorited_count,logs_count,included_answers_count,included_articles_count,included_text,message_thread_token,' \
+      f'account_status,is_active,is_bind_phone,is_force_renamed,is_bind_sina,is_privacy_protected,sina_weibo_url,' \
+      f'sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,is_org_createpin_white_user,' \
+      f'mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,' \
+      f'hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage'
+
+line_title_list = [
+    ('_date', '日期'),
+    ('voteup_count', "被点赞数"),
+    ('favorited_count', '被收藏数'),
+    ('thanked_count', '被感谢数'),
+    ('follower_count', "被关注数"),
+    ('following_count', '关注数'),
+    ('following_question_count', '关注的问题数'),
+    ('following_favlists_count', '关注的收藏夹数'),
+    ('following_columns_count', '关注的专栏数'),
+    ('following_topic_count', '关注的话题数'),
+    ('answer_count', '回答数'),
+    ('articles_count', '文章数'),
+    ('question_count', '提问数'),
+    ('pins_count', '想法数'),
+    ('participated_live_count', '赞助live次数'),
+    ('logs_count', '参与公共编辑次数'),
+    ('编辑推荐回答数', 'included_answers_count'),
+    ('编辑推荐文章数', 'included_articles_count'),
+    ('ip_info', 'ip属地'),
+
+    ('', ''),
+    ('', ''),
+    ('', ''),
+    ('', ''),
+    ('', ''),
+    ('', ''),
+    ('', ''),
+    # 未知
+    # favorite_count
+]
+
+
+def fetch_and_save():
+    resp = requests.get(url, headers=header)
+    data_dict = resp.json()
+
+    vote_up_count = data_dict['voteup_count']
+    follower_count = data_dict['follower_count']
+    answer_count = data_dict['answer_count']
+
+    date_str = datetime.datetime.now().strftime('%Y-%m-%d')
+
+    data_line = f"{date_str}, {vote_up_count}, {follower_count}\n"
+    append_file(FILE_NAME_DATA, data_line)
+    print(data_line)
+
+    js_str = json.dumps(data_dict, indent=4, ensure_ascii=False, sort_keys=True)
+    override_file(FILE_NAME_INFO, js_str)
+    print(js_str)
+
+
+def override_file(file_name, text):
+    with open(file_name, "w") as f:
+        f.write(text)
+
+
+def append_file(file_name, text):
+    with open(file_name, "a") as f:
+        f.write(text)
+
+
+if __name__ == '__main__':
+    fetch_and_save()
