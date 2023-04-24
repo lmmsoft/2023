a021885a22b5b7de103717a29e700614f2df2faf
lmm333 <lmm333@126.com>
2023-04-25 02:44:14 +0800

    FIX: 适配 bilibili.py 获取用户信息api异常导致的问题

--- bilibili.py
+++ bilibili.py
@@ -37,7 +37,26 @@ def append_file(file_name, text):
 
 def fetch(url, need_cookies=False):
     resp = requests.get(url, cookies=cookies if need_cookies else None, headers=header)
-    return resp.json()
+
+    # 请求个人信息，返回值异常，需要特殊处理，这里要做个特判
+    return handle_json_personal_info(resp.text)
+
+
+bad_case = '''{"code":-509,"message":"请求过于频繁，请稍后再试","ttl":1}{"code":0,"message":"0","ttl":1,"data":{"mid":385883467,"name":"明明如月成长笔记","sex":"保密","face":"https://i1.hdslb.com/bfs/face/c16020ea3a0e58d8d7ce3d52294add74c8d5dc2e.jpg","face_nft":0,"face_nft_type":0,"sign":"感觉自己萌萌哒~","rank":10000,"level":4,"jointime":0,"moral":0,"silence":0,"coins":0,"fans_badge":false,"fans_medal":{"show":false,"wear":false,"medal":null},"official":{"role":0,"title":"","desc":"","type":-1},"vip":{"type":2,"status":1,"due_date":1712505600000,"vip_pay_type":0,"theme_type":0,"label":{"path":"","text":"年度大会员","label_theme":"annual_vip","text_color":"#FFFFFF","bg_style":1,"bg_color":"#FB7299","border_color":"","use_img_label":true,"img_label_uri_hans":"","img_label_uri_hant":"","img_label_uri_hans_static":"https://i0.hdslb.com/bfs/vip/8d4f8bfc713826a5412a0a27eaaac4d6b9ede1d9.png","img_label_uri_hant_static":"https://i0.hdslb.com/bfs/activity-plat/static/20220614/e369244d0b14644f5e1a06431e22a4d5/VEW8fCC0hg.png"},"avatar_subscript":1,"nickname_color":"#FB7299","role":3,"avatar_subscript_url":"","tv_vip_status":0,"tv_vip_pay_type":0},"pendant":{"pid":0,"name":"","image":"","expire":0,"image_enhance":"","image_enhance_frame":""},"nameplate":{"nid":0,"name":"","image":"","image_small":"","level":"","condition":""},"user_honour_info":{"mid":0,"colour":null,"tags":[]},"is_followed":false,"top_photo":"http://i2.hdslb.com/bfs/space/cb1c3ef50e22b6096fde67febe863494caefebad.png","theme":{},"sys_notice":{},"live_room":null,"birthday":"","school":{"name":""},"profession":{"name":"","department":"","title":"","is_show":0},"tags":null,"series":{"user_upgrade_status":3,"show_upgrade_window":false},"is_senior_member":0,"mcn_info":null,"gaia_res_type":0,"gaia_data":null,"is_risk":false,"elec":{"show_info":{"show":false,"state":-1,"title":"","icon":"","jump_url":""}},"contract":null}}'''
+
+
+def handle_json_personal_info(text):
+    if '请求过于频繁，请稍后再试' in text:
+        idx = text.index('}')
+        text2 = text[idx + 1:]
+        js_dict = json.loads(text2)
+        return js_dict
+    else:
+        return json.loads(text)
+
+
+# 测试用
+# a = handle_json_personal_info(bad_case)
 
 
 json_personal_info = fetch(f'https://api.bilibili.com/x/space/acc/info?mid={UID}')  # 不要登陆
